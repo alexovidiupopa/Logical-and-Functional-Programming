1b1ac8417b3660fcdcf4e5cdfd6773b50d5e8724
Alex <popaalexovidiu@gmail.com>
2019-11-02 18:26:23 +0200

    lab2 done

--- P2/7.pl
+++ P2/7.pl
@@ -1,4 +1,3 @@
-%Get the maximum from the list
 %maxList(L:list, M:int, R:int)
 %maxList(i,i,o)
 maxList([],M,M).
@@ -7,20 +6,11 @@ maxList([H|T],M,R):- H > M, !,
 maxList([_|T],M,R):-
     maxList(T,M,R).
 
-% Wrapper function to call the maximum list function with the first
-% element
 %maxListWrapper(L:list, R:list)
 %maxListWrapper(i,o)
 maxListWrapper([H|T],R):- maxList(T,H,R).
 
-%lengthOfList(L:list, R:int)
-%lengthOfList(i,o)
-lengthOfList([],0).
-lengthOfList([_|T], R):- lengthOfList(T,R1),
-    R is R1+1.
 
-% Given the max from a list, compute a list which has all the positions
-% on which the maximum appears
 %maxPositions(L:list, MAX:int, P:int,R:list)
 %maxPositions(i,i,i,o)
 maxPositions([],_,_,[]).
@@ -32,9 +22,6 @@ maxPositions([_|T],MAX,P, R):-
     P1 is P+1,
     maxPositions(T,MAX,P1, R).
 
-% Wrapper to call the max positions function with the initial position 0(
-% to be made into 1 after the first
-% call
 %maxPositionsWrapper(L:list,R:list)
 %maxPositionsWrapper(i,o)
 maxPositionsWrapper([],[]).
@@ -42,18 +29,12 @@ maxPositionsWrapper([H|T],R):-
     maxListWrapper([H|T],MAX),
     maxPositions([H|T],MAX,0,R).
 
-% Replace the sublists with the max positions list if their length>1,
-% else add the solo elem to the list
 % replaceLists(L:list,R:list)
 % replaceLists(i,o)
 replaceLists([],[]).
 replaceLists([H|T],[H|R]):- not(is_list(H)),
     replaceLists(T,R).
-replaceLists([H|T],[H|R]):- lengthOfList(H,L),
-    L is 1,
-    replaceLists(T,R).
-replaceLists([H|T],R):- lengthOfList(H,L),
-    L > 1,
+replaceLists([H|T],R):-
     maxPositionsWrapper(H,NEW),
     replaceLists(T,R1),
     R = [NEW|R1].
